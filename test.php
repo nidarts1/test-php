d7611805807fde64dab9a645d6380d5bd4e1bac2
nidarts1 <82223134+nidarts1@users.noreply.github.com>
2022-03-15 17:50:56 -0400

    Add files via upload

---
+++ test.php
@@ -0,0 +1,18 @@
+<html>
+    
+    <head>
+        
+       <title> test php</title> 
+        
+    </head>
+  <body>
+    <?php echo'<p> bonjour</p>' ; ?>
+      
+  </body>  
+    
+    
+    
+    
+    
+    
+</html>
